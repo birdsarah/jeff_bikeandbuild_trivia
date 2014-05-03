c52bcbb6c0e9000882b803c9d9ca253e51d0b80e
Sarah Bird <sarah@bonvaya.com>
2014-05-02 20:02:02 -0700

    Add media_url

--- bikeandbuild/settings.py
+++ bikeandbuild/settings.py
@@ -172,3 +172,4 @@ if DEBUG is False:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL
+    MEDIA_URL = '%smedia/uploads/' % S3_URL
